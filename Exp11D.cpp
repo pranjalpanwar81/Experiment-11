9943383d5eb97f858aad1290790ffaf715639bfe
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-09-11 08:43:38 +0530

    Create Exp11D.cpp

---
+++ Exp11D.cpp
@@ -0,0 +1,35 @@
+// NAME - Pranjal Panwar 
+// PRN - 23070123164
+// EXPERIMENT -- 11(D) 
+
+# include<iostream>
+using namespace std;
+class cuboid
+{
+    private:
+    double h = 2.0,b = 3.0,l = 4.0;
+    public:
+    double volume()
+    {
+        double vol;
+        vol = h * b * l;
+        return vol;
+    }
+
+    void disp(double vol)
+    {
+        cout<<"Volume "<<vol<<endl;
+
+    }
+
+};
+int main()
+{
+    cuboid c1;
+    double v = c1.volume();
+    c1.disp(v);
+  
+}
+
+// Output  
+// Volume 24        
