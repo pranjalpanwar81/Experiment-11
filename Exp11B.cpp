bef0a9cea4aadbf713345e30b53a030a6c388416
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-09-11 08:40:42 +0530

    Update Exp11B.cpp

---
+++ Exp11B.cpp
@@ -0,0 +1,26 @@
+// NAME - Pranjal Panwar
+// PRN - 23070123164
+// EXPERIMENT - 11(B) 
+
+# include<iostream>
+using namespace std;
+class cuboid
+{
+    public:
+    double h = 2.0,b = 3.0,l = 4.0;
+    double volume()
+    {
+        double vol = h * b * l;
+        cout<<"Volume "<<vol<<endl;
+    }
+
+};
+int main()
+{
+    cuboid c1;
+    c1.volume();
+  
+}
+
+// Output 
+// Volume 24 
